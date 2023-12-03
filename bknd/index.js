e08ff082aebf04315e546545be2fd14ba6d64a36
Raghvendra Tiwari <98442936+raghav1482@users.noreply.github.com>
2023-12-03 13:52:37 +0530

    Update index.js

--- bknd/index.js
+++ bknd/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin:["","http://localhost:3000"],
+        origin:["https://raghavhelivers.vercel.app","http://localhost:3000"],
         credentials:true
     }
 ));
